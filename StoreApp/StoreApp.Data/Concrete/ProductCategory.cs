6accccad303635cdd974ed30247ce0e116e79aa8
Sadık Turan <sadikturangmail.com>
2024-02-21 13:38:57 +0300

    razor pages projesi

--- StoreApp/StoreApp.Data/Concrete/ProductCategory.cs
+++
@@ -1,7 +0,0 @@
-namespace StoreApp.Data.Concrete;
-
-public class ProductCategory
-{
-    public int ProductId { get; set; }
-    public int CategoryId { get; set; }
-}
--- StoreApp/StoreApp.Web/Models/CategoryViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace StoreApp.Web.Models;
-
-public class CategoryViewModel
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Url { get; set; } = string.Empty; 
-}
