f940742db19b33006b1c86c341096403695e9eb4
Sadık Turan <sadikturangmail.com>
2024-02-23 09:10:19 +0300

    routing

---
+++ razorpagesExample/Pages/Employees.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorpagesExample.Pages;
+
+public class EmployeesModel : PageModel
+{
+       public void OnGet()
+    {
+
+    }
+
+}
---
+++ razorpagesExample/Pages/Employees/Details.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorpagesExample.Pages.Employees;
+
+public class DetailsModel: PageModel
+{
+
+}
