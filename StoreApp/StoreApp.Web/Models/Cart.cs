08d5732ab2bbf656bd7952f2ca5cb7271896ad21
Sadık Turan <sadikturangmail.com>
2024-02-25 22:23:50 +0300

    cart model

---
+++ StoreApp/StoreApp.Web/Models/Cart.cs
@@ -0,0 +1,44 @@
+﻿using StoreApp.Data.Concrete;
+
+namespace StoreApp.Web;
+
+public class Cart
+{
+    public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+    public void AddItem(Product product, int quantity)
+    {
+        var item = Items.Where(p => p.Product.Id == product.Id).FirstOrDefault();
+
+        if (item == null)
+        {
+            Items.Add(new CartItem { Product = product, Quantity = quantity });
+        }
+        else
+        {
+            item.Quantity += quantity;
+        }
+    }
+
+    public void RemoveItem(Product product)
+    {
+        Items.RemoveAll(i => i.Product.Id == product.Id);
+    }
+
+    public decimal CalculateTotal()
+    {
+        return Items.Sum(i => i.Product.Price * i.Quantity);
+    }
+
+    public void Clear()
+    {
+        Items.Clear();    
+    }
+}
+
+public class CartItem
+{
+    public int CartItemId { get; set; }
+    public Product Product { get; set; } = new();
+    public int Quantity { get; set; }
+}
