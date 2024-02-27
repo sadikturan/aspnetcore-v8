622fbce3ad3fc769f4c016cf4cddb7e1093bd411
Sadık Turan <sadikturangmail.com>
2024-02-27 12:44:25 +0300

    cart summary

---
+++ StoreApp/StoreApp.Web/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Web.Models;
+
+namespace StoreApp.Web.Components;
+
+public class CartSummaryViewComponent:ViewComponent
+{
+    private Cart cart;
+
+    public CartSummaryViewComponent(Cart cartSevice)
+    {
+        cart = cartSevice;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        return View(cart);
+    }
+}
